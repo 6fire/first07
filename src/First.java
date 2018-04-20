0030af8a703593bc6b83f44c908e7d3143b4a23a
Administrator <sdf@sfd>
2018-04-20 11:10:13 +0800

    第5次提交 sum()

--- src/First.java
+++ src/First.java
@@ -1,7 +1,7 @@
 public class First {
     private  int x;
-    public int sum(int x,int y){return x+y;}
+    public static int sum(int x,int y){return x+y;}
     public static void main(String[] args) {
-
+  sum(3,43);
     }
 }
